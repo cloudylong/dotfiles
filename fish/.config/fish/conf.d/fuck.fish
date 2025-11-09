e48ed9091cdd01ba7afde166f566a71026f12dd0
Walter <walter.yl.wang@outlook.com>
2025-11-09 19:23:13 +0800

    Fix: 修改 Fish 中 thefuck 的配置

---
+++ fish/.config/fish/conf.d/fuck.fish
@@ -0,0 +1,2 @@
+# 不喜欢控制台上出现一大坨色块
+set -gx THEFUCK_NO_COLORS true
--- fish/.config/fish/functions/fuck.fish
+++ fish/.config/fish/functions/fuck.fish
@@ -1,6 +1,9 @@
-function fuck
-    thefuck --alias | source 
-
-    # 不喜欢控制台上出现一大坨色块
-    set -gx THEFUCK_NO_COLORS true
+function fuck -d "Correct your previous console command"
+  set -l fucked_up_command $history[1]
+  env TF_SHELL=fish TF_ALIAS=fuck PYTHONIOENCODING=utf-8 thefuck $fucked_up_command THEFUCK_ARGUMENT_PLACEHOLDER $argv | read -l unfucked_command
+  if [ "$unfucked_command" != "" ]
+    eval $unfucked_command
+    builtin history delete --exact --case-sensitive -- $fucked_up_command
+    builtin history merge
+  end
 end
